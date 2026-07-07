c67eff5ac21f7250812e59b4e487cc1e174827f8
noodles <18706334023@163.com>
2026-07-07 12:44:02 +0800

    R260707

--- diy2-pw.sh
+++ diy2-pw.sh
@@ -23,5 +23,5 @@ sed -i 's/ImmortalWrt/noodlesWrt/g' package/base-files/files/bin/config_generate
 # sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=AP-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-|' include/image.mk
 sed -i "s|\$(VERSION_DIST_SANITIZED)-|\$(VERSION_DIST_SANITIZED)-$(TZ=Asia/Shanghai date +%Y%m%d)-pw-|" include/image.mk
 
-sed -i '/sed -i "\/log-facility\/d" "\/etc\/dnsmasq.conf"/i sed -i '\''/BUILD_ID/d'\'' /etc/os-release\necho "BUILD_ID='\''R260630'\''" >> /etc/os-release' package/emortal/default-settings/files/99-default-settings
+sed -i '/sed -i "\/log-facility\/d" "\/etc\/dnsmasq.conf"/i sed -i '\''/BUILD_ID/d'\'' /etc/os-release\necho "BUILD_ID='\''R260707'\''" >> /etc/os-release' package/emortal/default-settings/files/99-default-settings
 
